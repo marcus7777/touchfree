d725c288777227559c62a45115670b5e7ce517c4
Marcus Hitchins <marcus@hhost.me>
2021-09-10 15:46:10 +0100

    cors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const exec = require('child_process').exec
 const express = require('express')
+const cors = require('cors')
+var app = express()
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get('/', cors({origin: '*'}), (req, res) => {
   res.send('Hello World!')
   exec('sh ~/press.sh', function (error, stdout, stderr) {})
 })
